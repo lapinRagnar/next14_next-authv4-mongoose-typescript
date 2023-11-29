8220e1634d39d47135e5d03eeb11ceec1c648e3d
lapinRagnar <lapinragnar@gmail.com>
2023-11-29 20:20:49 +0100

    proteger la page dashboard si l'user n'est connecté on redirige vers /

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,19 @@
+import {getServerSession} from "next-auth"
+import { redirect } from "next/navigation"
 
-const Dashboard = () => {
+const Dashboard = async () => {
+  
+  const session = await getServerSession()
+  
+  if (!session) {
+    redirect("/");
+  }
+  
   return (
-    <div>Dashboard</div>
+    <div className="flex min-h-screen flex-col items-center justify-start p-24">
+      <h1 className="text-3xl mb-5">Tableau de bord</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, consequuntur. Recusandae aspernatur ducimus eligendi laborum asperiores explicabo itaque est rerum iure error iusto, eaque illo perspiciatis consectetur quisquam! Rerum, deserunt?</p>
+    </div>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Page Home</h1>
+    <main className="flex min-h-screen flex-col items-center justify-start p-24">
+      <h1 className='text-3xl mb-5'>Bienvenue</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus facere qui perspiciatis repellat sequi iure laborum illo culpa deleniti. Molestias quae esse sapiente voluptatum officia rem sed odio saepe voluptatibus.</p>
     </main>
   )
